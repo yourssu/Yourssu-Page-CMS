564cd4714cb0b94173920a02dbd2b291e08ab334
aaminha <dks4857@gmail.com>
2024-02-11 21:59:35 +0900

    feat: 스키마 설정

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,20 @@
+import applyStepContent from './applyStepContent'
+import defaultContent from './defaultContent'
+import department from './department'
+import inaWordContent from './inaWordContent'
+import informationContent from './informationContent'
+import roadToProContent from './roadToProContent'
+import skillContent from './skillContent'
+
 export const schemaTypes = [
   // Document types
+  department,
+
   // Other types
+  defaultContent,
+  skillContent,
+  informationContent,
+  applyStepContent,
+  roadToProContent,
+  inaWordContent,
 ]
