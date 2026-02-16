39d2fe733ba1e0fd188bddd4d3168d2d127991bf
wwwlnyy <wwwlnyy@gmail.com>
2026-02-16 18:23:35 +0900

    refactor: deafultContent > 소제목 입력방법 설명 문구 추가

--- schemas/Types/defaultContent.ts
+++ schemas/Types/defaultContent.ts
@@ -12,6 +12,7 @@ export default defineType({
     defineField({
       name: 'content',
       title: '내용',
+      description: '소제목은 [소제목] 형식으로 입력해주세요.',
       type: 'array',
       of: [{type: 'string'}],
     }),
